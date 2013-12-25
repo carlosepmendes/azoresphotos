d6cc5920e75b22beb4e9e5eafdd87a8192f7adc9
carlosepmendes <carlosepmendes@gmail.com>
2013-12-25 21:12:11 -0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Azoresphotos::Application.config.secret_token = '181750cfb88bf47fb8cfe651e6e91a01a83086aaeb444d10d7652a6f8c7459d07cf9158a41873636385ff43c1783f41fa2f8becec1ba670fa809a2c85f19d6bc'
